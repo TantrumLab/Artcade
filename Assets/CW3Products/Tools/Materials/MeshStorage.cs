18921ce6fd249b1fcd68dda8581098bb2328a0ad
Eric Mouledoux <EricMouledoux@gmail.com>
2017-04-28 17:12:54 -0500

    Cave demo scene

--- Assets/CW3Products/Tools/Materials/MeshStorage.cs
+++ Assets/CW3Products/Tools/Materials/MeshStorage.cs
@@ -8,30 +8,27 @@ using UnityEngine;
 
 public class MeshStorage : MonoBehaviour
 {
-    //This is the version of this script for the simplified version that deletes itself
-#if UNITY_EDITOR
+    
+    #if UNITY_EDITOR
     public Mesh originalMesh;
     public bool ignoreVertexInfluenceSpheres = false;
 
     void Awake()
     {
-        if (!Application.isPlaying)
+        if( !Application.isPlaying )
         {
-            if (!transform.name.Contains("Fluids_"))
+            if (originalMesh == null)
             {
                 MeshFilter meshFilter = GetComponent<MeshFilter>();
-                meshFilter.sharedMesh = originalMesh;
-                FreeformRockScalar rockscalar = GetComponent<FreeformRockScalar>();
-                if (rockscalar != null)
+                if (meshFilter.sharedMesh.name != "VPaintedMesh")
                 {
-                    DestroyImmediate(rockscalar);
+                    originalMesh = meshFilter.sharedMesh;
                 }
-                DestroyImmediate(this);
             }
         }
     }
 
-#endif
+    #endif
 }
 
 
