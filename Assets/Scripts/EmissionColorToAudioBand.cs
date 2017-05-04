ab7d73959d8a6f6ae9558d57bd0a73117edd5d13
Eric Mouledoux <EricMouledoux@gmail.com>
2017-05-04 18:55:54 -0500

    Emission color to audio script

--- Assets/Scripts/EmissionColorToAudioBand.cs
+++ Assets/Scripts/EmissionColorToAudioBand.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorToAudioBand : MonoBehaviour
+public class EmissionColorToAudioBand : MonoBehaviour
 {
     [SerializeField]
     private AudioVisualization m_AV;
@@ -11,7 +11,8 @@ public class ColorToAudioBand : MonoBehaviour
     private int m_AudioBand;
 
     [SerializeField]
-    private Color m_Color;
+    private Color m_NewColor;
+    private Color m_OriginalColor;
 
     private Material m_Material;
 
@@ -20,11 +21,13 @@ public class ColorToAudioBand : MonoBehaviour
     {
         m_Material = new Material(GetComponent<Renderer>().material);
         GetComponent<Renderer>().material = m_Material;
+        m_OriginalColor = m_Material.GetColor("_EmissionColor");
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        float frequency = m_AV.m_CurrentFrequencyStereo[m_AudioBand];
+        m_Material.SetColor("_EmissionColor", Color.Lerp(m_OriginalColor, m_NewColor, frequency));
 	}
 }
