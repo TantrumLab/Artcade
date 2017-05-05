77df3fb5e15be3d703f2950141e842abad6fd7fc
Eric Mouledoux <EricMouledoux@gmail.com>
2017-05-05 14:06:51 -0500

    Crystals now light up instead of changing color

---
+++ Assets/Scripts/GemFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemFollow : MonoBehaviour
+{
+    public Transform m_Target;
+    public Vector3 m_Offset;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (m_Target == null)
+            Destroy(gameObject);
+        else
+        {
+            transform.position = Vector3.Lerp(
+                transform.position, m_Target.transform.position + m_Offset, Time.deltaTime);
+        }
+	}
+}
