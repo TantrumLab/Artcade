a86db2de5d6c1942d96ff0be7cbe50a1e69dc12b
Eric Mouledoux <EricMouledoux@gmail.com>
2017-05-08 10:06:34 -0500

    Secrete audio and enemies are now smaller

--- Assets/Scripts/DestroyOnAudioEnd.cs
+++ Assets/Scripts/DestroyOnAudioEnd.cs
@@ -6,12 +6,26 @@ using UnityEngine;
 public class DestroyOnAudioEnd : MonoBehaviour
 {
     private AudioSource m_AudioSource;
+
+    [SerializeField]
+    private AudioClip m_SecreteClip;
+
+    [SerializeField]
+    private int m_ClipRarety;
 	// Use this for initialization
 	void Start ()
     {
         m_AudioSource = GetComponent<AudioSource>();
+
+        if (Random.Range(0, m_ClipRarety) == 0)
+        {
+            m_AudioSource.clip = m_SecreteClip;
+        }
+
         m_AudioSource.playOnAwake = true;
         m_AudioSource.loop = false;
+
+
 	}
 	
 	// Update is called once per frame
